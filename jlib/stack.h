c4ed4a5fa1c7cb44782aabd7d8fffbcd4ba6eaa1
Josu <56826352+josugoar@users.noreply.github.com>
2021-04-25 11:06:56 +0200

    Add parameter names to function prototypes

--- jlib/stack.h
+++ jlib/stack.h
@@ -2,10 +2,10 @@
 
 struct Stack;
 
-struct Stack *stack_new(size_t);
+struct Stack *stack_new(size_t elt_size);
 
-void stack_free(struct Stack *);
+void stack_free(struct Stack *stack);
 
-struct Stack *stack_push(struct Stack *, void *);
+struct Stack *stack_push(struct Stack *stack, void *item);
 
-struct Stack *stack_pop(struct Stack *, void *);
+struct Stack *stack_pop(struct Stack *stack, void *item);
